0d38ae892b1cc1d2b74c8ab013e4f749e78dc09d
Riddhesh Veling <velingriddhesh@gmail.com>
2023-12-28 10:10:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.stopwatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.stopwatch.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var isRunning = false
+    private var startTimeMillis = 0L
+
+    private val handler = Handler(Looper.getMainLooper())
+    private val runnable = object : Runnable{
+        override fun run() {
+            val currentTimeMillis = System.currentTimeMillis()
+            val elapsedMillis = currentTimeMillis - startTimeMillis
+
+            val minutes = (elapsedMillis / 1000) / 60
+            val seconds = (elapsedMillis / 1000) % 60
+            val milliseconds = elapsedMillis % 1000
+
+            val time = String.format("%02d:%02d:%03d", minutes, seconds, milliseconds)
+            binding.timerText.text = time
+
+            handler.postDelayed(this, 1)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener {
+            startTimer()
+        }
+        binding.stopBtn.setOnClickListener {
+            stopTimer()
+        }
+        binding.resetBtn.setOnClickListener {
+            resetTimer()
+        }
+    }
+
+    private fun startTimer(){
+        if (!isRunning){
+            startTimeMillis = System.currentTimeMillis()
+            handler.postDelayed(runnable, 1)
+            isRunning = true
+
+            binding.startBtn.isEnabled = false
+            binding.stopBtn.isEnabled = true
+            binding.resetBtn.isEnabled = true
+        }
+    }
+
+    private fun stopTimer(){
+        if(isRunning){
+            handler.removeCallbacks(runnable)
+            isRunning = false
+
+            binding.startBtn.isEnabled = true
+            binding.startBtn.text = "Resume"
+            binding.stopBtn.isEnabled = false
+            binding.resetBtn.isEnabled = true
+        }
+    }
+
+    private fun resetTimer(){
+        stopTimer()
+
+        startTimeMillis = 0
+        binding.timerText.text = "00:00:000"
+
+        binding.startBtn.isEnabled = true
+        binding.stopBtn.isEnabled = false
+        binding.startBtn.text = "Start"
+    }
+}
