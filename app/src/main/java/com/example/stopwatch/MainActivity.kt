309d9c8551a9540319f3d33cd666002355c1fec6
Riddhesh Veling <velingriddhesh@gmail.com>
2023-12-28 11:21:03 +0530

    Start button updated

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -11,16 +11,17 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var isRunning = false
     private var startTimeMillis = 0L
+    private var timerMillis = 0L
 
     private val handler = Handler(Looper.getMainLooper())
-    private val runnable = object : Runnable{
+    private val runnable = object : Runnable {
         override fun run() {
             val currentTimeMillis = System.currentTimeMillis()
-            val elapsedMillis = currentTimeMillis - startTimeMillis
+            timerMillis = currentTimeMillis - startTimeMillis
 
-            val minutes = (elapsedMillis / 1000) / 60
-            val seconds = (elapsedMillis / 1000) % 60
-            val milliseconds = elapsedMillis % 1000
+            val minutes = (timerMillis / 1000) / 60
+            val seconds = (timerMillis / 1000) % 60
+            val milliseconds = timerMillis % 1000
 
             val time = String.format("%02d:%02d:%03d", minutes, seconds, milliseconds)
             binding.timerText.text = time
@@ -45,9 +46,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun startTimer(){
-        if (!isRunning){
-            startTimeMillis = System.currentTimeMillis()
+    private fun startTimer() {
+        if (!isRunning) {
+            startTimeMillis = System.currentTimeMillis() - timerMillis
             handler.postDelayed(runnable, 1)
             isRunning = true
 
@@ -57,8 +58,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun stopTimer(){
-        if(isRunning){
+    private fun stopTimer() {
+        if (isRunning) {
             handler.removeCallbacks(runnable)
             isRunning = false
 
@@ -69,10 +70,11 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun resetTimer(){
+    private fun resetTimer() {
         stopTimer()
 
         startTimeMillis = 0
+        timerMillis = 0
         binding.timerText.text = "00:00:000"
 
         binding.startBtn.isEnabled = true
